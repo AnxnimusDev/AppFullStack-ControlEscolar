2d972938a2bf1c56e151dacfbc48b026fca1474f
Adrián Blanco (Anxnimus) <ablancodevelop@gmail.com>
2024-04-02 10:13:40 +0200

    Poquito más de CSS (creo)

---
+++ TestingProject/src/main/java/com/mycompany/testingproject/TestingProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingproject;
+
+/**
+ *
+ * @author anxnimus
+ */
+public class TestingProject {
+    
+    public static int sumar(int a, int b){
+        return 0;
+    }
+}
---
+++ TestingProject/src/test/java/TestingClass.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.mycompany.testingproject.TestingProject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author anxnimus
+ */
+public class TestingClass {
+    
+    public TestingClass() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testSumar(int a, int b){
+        int result = TestingProject.sumar(2, 3);
+        assertEquals(5, result);
+    }
+}
